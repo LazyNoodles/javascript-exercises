a75a9aea5c7f97e6393ea85e0dd80941ae3c610b
LazyNoodles <144541228+LazyNoodles@users.noreply.github.com>
2024-09-15 17:39:46 +0700

    Finised the exercies 1-6

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let split = string.split("");
+let reverse = split.reverse();
+let joinArray = reverse.join("");
+return joinArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let newTemp = (fahrenheit - 32) * (5/9);
+  let rounded = Math.round(newTemp * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let newTemp = (9/5) * celsius + 32;
+  let rounded = Math.round(newTemp * 10) / 10;
+  return rounded; 
 };
 
 // Do not edit below this line
